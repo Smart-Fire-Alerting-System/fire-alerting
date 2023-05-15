c8eb27e69f5bd5d4e977addc9b92c766b1d8e30b
ndtuan <ndtuanftd@gmail.com>
2023-05-15 10:11:33 +0700

    [fix] Remove request header

--- pages/api/api.js
+++ pages/api/api.js
@@ -3,17 +3,16 @@ import axios from "axios";
 
 const API = axios.create({ baseURL: "http://localhost:5000" });
 
-API.interceptors.request.use((req) => {
-//   if (localStorage.getItem("user")) {
-//     req.headers.Authorization = `Bearer ${
-//       JSON.parse(localStorage.getItem("user")).token
-//     }`;
-//   }
-  const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NDVmNmMwYjBlYTcyNmQ5ZDMyZDQ1OGQiLCJpYXQiOjE2ODM5Nzc4NDcsImV4cCI6MTY4NjU2OTg0N30.T0Ou5IBN5Tg22uUzMA32b678tl1cfOwYo-6qvWrYNJQ"
-  req.headers.Authorization = `Bearer ${token}`;
-  console.log(req.headers.Authorization);
-  return req;
-});
+// API.interceptors.request.use((req) => {
+// //   if (localStorage.getItem("user")) {
+// //     req.headers.Authorization = `Bearer ${
+// //       JSON.parse(localStorage.getItem("user")).token
+// //     }`;
+// //   }
+//   const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NDVmNmMwYjBlYTcyNmQ5ZDMyZDQ1OGQiLCJpYXQiOjE2ODM5Nzc4NDcsImV4cCI6MTY4NjU2OTg0N30.T0Ou5IBN5Tg22uUzMA32b678tl1cfOwYo-6qvWrYNJQ"
+//   req.headers.Authorization = `Bearer ${token}`;
+//   return req;
+// });
 
 export const loginUser = (dataForm) => API.post("/api/user/login", dataForm);
 
